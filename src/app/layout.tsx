866f381f34cec167b55f24835767b8a3e32bcec0
Ravi Kant <kumarra927@gmail.com>
2025-10-09 21:11:27 +0530

    First push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "./components/layout/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <div className="flex flex-col min-h-screen">
+            <Navbar/>
+          <main className="flex flex-col flex-grow container mx-auto px-4 py-4">{children}</main>
+        </div>
       </body>
     </html>
   );
